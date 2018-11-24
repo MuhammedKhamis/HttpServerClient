eaa8982f2b61ad11ee594fd77c1b089f62101c68
MuhammedKhamis <muhammedessam10@gmail.com>
2018-11-24 21:47:18 +0200

    modifiying Client main

--- Client/main.cpp
+++ Client/main.cpp
@@ -10,7 +10,7 @@
 /* main function */
 /*************************************/
 
-static void connectionStatus(int status){
+void connectionStatus(int status){
     if(status == -1){
         cout << "Connecting Disconnected\n";
     }
@@ -18,46 +18,11 @@ static void connectionStatus(int status){
 
 static void* runThread(void *p){
 
-    // where to store data on client module
-    string dataDirectory = ".";
 
-    HttpClient client(dataDirectory);
-
-    // reading args
-    int port = 8000;
-    char *serverAddress = "127.0.0.1";
-     client.connectionInit(serverAddress, port);
-
-     // scan user command
-     vector<string> userCommand = {"GET c.jpg 127.0.0.1"};
-
-     //printf("Enter Command: ");
-     //getline(cin,userCommand);
-     vector<Request> reqs;
-    int status;
-    for(string command : userCommand) {
-         // parse user command
-          Request requestObj = Parser::parseInputCommand(command);
-          if(requestObj.getMethod() == GET){
-              reqs.push_back(requestObj);
-          }else{
-              status = client.sendGETRequests(reqs);
-              connectionStatus(status);
-
-              status = client.sendPOSTRequest(requestObj);
-              connectionStatus(status);
-          }
-     }
-    status = client.sendGETRequests(reqs);
-    connectionStatus(status);
-    client.closeConnection();
     return NULL;
 }
 
-
-int main(int argc, char *argv[])
-{
-
+int testMain(){
     int sz = 1;
     pthread_t ts[sz];
     for(int i = 0 ; i < sz ; i++){
@@ -70,5 +35,58 @@ int main(int argc, char *argv[])
         free(status);
     }
 
-    return 0; 
+    return 0;
+}
+
+int applyCommands(vector<string> userCommands, HttpClient client){
+
+    vector<Request> reqs;
+    int status;
+    for(string command : userCommands) {
+        // parse user command
+        Request requestObj = Parser::parseInputCommand(command);
+        if(requestObj.getMethod() == GET){
+            reqs.push_back(requestObj);
+        }else{
+            status = client.sendGETRequests(reqs);
+            connectionStatus(status);
+
+            status = client.sendPOSTRequest(requestObj);
+            connectionStatus(status);
+        }
+    }
+    status = client.sendGETRequests(reqs);
+    connectionStatus(status);
+    client.closeConnection();
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // input shape ./client server-ip port-number file-name
+    // where to store data on client module
+    string dataDirectory = ".";
+    HttpClient client(dataDirectory);
+
+
+    // reading args
+    int port = atoi(argv[2]);
+    char *serverAddress = argv[1];
+    char *fileName = argv[3];
+    vector<string> userCommands;
+
+    string command;
+    ifstream myfile (fileName);
+
+    if(myfile.is_open()){
+        while ( getline (myfile,command) )
+        {
+            userCommands.push_back(command);
+            cout << command << endl;
+        }
+        myfile.close();
+    }
+
+    client.connectionInit(serverAddress, port);
+    applyCommands(userCommands, client);
 }
